5222ebed027173f626c210c7e4ddf970d6721da6
Ian <egian93@gmail.com>
2021-12-08 03:52:19 -0600

    db.js added

---
+++ public/db.js
@@ -0,0 +1,65 @@
+let db;
+const request = window.indexedDB.open('budget-tracker', 1);
+
+function checkForIndexedDb() {
+  indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+
+  if (!window.indexedDB) {
+    console.log(`Your browser doesn't support a stable version of IndexedDB.`);
+    return false;
+  }
+  return true;
+}
+
+request.onupgradeneeded = ({ target }) => {
+  db = target.result;
+  const pendingStore = db.createObjectStore('PendingStore', { autoIncrement: true });
+};
+
+request.onsuccess = ({ target }) => {
+  db = target.result;
+
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = function(e) {
+  console.log(`Error: ${e.target.errorCode}`)
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(['PendingStore'], 'readwrite');
+  const store = transaction.objectStore('PendingStore');
+
+  store.add(record);
+}
+
+function checkDatabase() {
+  const transaction = db.transaction(['PendingStore'], 'readwrite');
+  const store = transaction.objectStore('PendingStore');
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+      .then((response) => response.json())
+      .then(() => {
+        if (res.length !== 0) {
+          transaction = db.transaction(['PendingStore'], 'readwrite');
+          const currentStore = transaction.objectStore('PendingStore');
+          currentStore.clear();
+        }
+      });
+    }
+  };
+}
+
+window.addEventListener('online', checkDatabase);
